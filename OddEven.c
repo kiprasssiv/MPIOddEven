8d0b804a9152f96e0925d4151c1e8d302c3cd606
KiprasSivickas <sivickaskipras@gmail.com>
2020-05-25 05:09:37 +0300

    first commit

--- OddEven.c
+++ OddEven.c
@@ -1,145 +1,124 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <mpi.h>
-int* randomArray(int *array, int size){
-  int i;
-  array = malloc(size * sizeof(int));
-  srand(time(0));
+void Sort(int local_A[], int local_n, int my_rank,
+         int p, MPI_Comm comm) {
+   int phase;
+   int *temp_B, *temp_C;
+   int even_partner;  /* phase is even or left-looking */
+   int odd_partner;   /* phase is odd or right-looking */
 
-  for(i = 0; i < size; i++){
-    array[i] = rand() % size;
-  }
-  return array;
-}
+   /* Temporary storage used in merge-split */
+   temp_B = (int*) malloc(local_n*sizeof(int));
+   temp_C = (int*) malloc(local_n*sizeof(int));
 
-void printArray(int *array, int size){
-  int i;
-  for(i = 0; i < size; i++){
-    printf("%d ", array[i]);
-  }
-  printf("\n");
-}
-//
-void mpiSort(int worldRank, int subArraySize, int *originalArray, int *sorted){
-  int sortedArray = 0;
-  int odd = 0;
+   /* Find partners:  negative rank => do nothing during phase */
+   if (my_rank % 2 != 0) {   /* odd rank */
+      even_partner = my_rank - 1;
+      odd_partner = my_rank + 1;
+      if (odd_partner == p) odd_partner = MPI_PROC_NULL;  // Idle during odd phase
+   } else {                   /* even rank */
+      even_partner = my_rank + 1;
+      if (even_partner == p) even_partner = MPI_PROC_NULL;  // Idle during even phase
+      odd_partner = my_rank-1;
+   }
 
-  while(sortedArray == 0){
-        //even time
-    if(odd == 0){
-            sortedArray = 1;
-        for(int i=worldRank*subArraySize+odd;i<worldRank*subArraySize+subArraySize;i+=2){
-            if(originalArray[i]>originalArray[i+1]){
-                int temp = originalArray[i];
-                originalArray[i] = originalArray[i+1];
-                originalArray[i+1] = temp;
-                sortedArray = 0;
-            }
-        }
-        odd = 1;
-    }
-        //odd time
-    else{
-            sortedArray = 1;
-        for(int i=worldRank*subArraySize+odd;i<worldRank*subArraySize+subArraySize;i+=2){
-            if(originalArray[i]>originalArray[i+1]){
-                int temp = originalArray[i];
-                originalArray[i] = originalArray[i+1];
-                originalArray[i+1] = temp;
-                sortedArray = 0;
-            }
-        }
-        odd = 0;
-    }
-    MPI_Barrier(MPI_COMM_WORLD);
-  }
+   /* Sort local list using built-in quick sort */
+   qsort(local_A, local_n, sizeof(int), Compare);
 
-}
+#  ifdef DEBUG
+   printf("Proc %d > before loop in sort\n", my_rank);
+   fflush(stdout);
+#  endif
 
-int isCorrect(int *array, int arraySize){
-  int i;
-  for(i = 0; i < arraySize - 1; i++){
-    if(array[i] > array[i + 1]){
-      printf("ERROR, sorted not correctly");
-      return 0;
-    }
-  }
-  return 1;
-}
+   for (phase = 0; phase < p; phase++)
+      Odd_even_iter(local_A, temp_B, temp_C, local_n, phase,
+             even_partner, odd_partner, my_rank, p, comm);
 
+   // deallocate memory
+   free(temp_B);
+   free(temp_C);
+}  /* Sort */
+void Odd_even_iter(int local_A[], int temp_B[], int temp_C[],
+        int local_n, int phase, int even_partner, int odd_partner,
+        int my_rank, int p, MPI_Comm comm) {
+   MPI_Status status;
 
+   if (phase % 2 == 0) { /* even phase */
+      if (even_partner >= 0) { /* check for even partner */
+         MPI_Sendrecv(local_A, local_n, MPI_INT, even_partner, 0,
+            temp_B, local_n, MPI_INT, even_partner, 0, comm,
+            &status);
+         if (my_rank % 2 != 0) /* odd rank */
+            // local_A have largest local_n ints from local_A and even_partner
+            Merge_high(local_A, temp_B, temp_C, local_n);
+         else /* even rank */
+            // local_A have smallest local_n ints from local_A and even_partner
+            Merge_low(local_A, temp_B, temp_C, local_n);
+      }
+   } else { /* odd phase */
+      if (odd_partner >= 0) {  /* check for odd partner */
+         MPI_Sendrecv(local_A, local_n, MPI_INT, odd_partner, 0,
+            temp_B, local_n, MPI_INT, odd_partner, 0, comm,
+            &status);
+         if (my_rank % 2 != 0) /* odd rank */
+            Merge_low(local_A, temp_B, temp_C, local_n);
+         else /* even rank */
+            Merge_high(local_A, temp_B, temp_C, local_n);
+      }
+   }
+}  /* Odd_even_iter */
+int main(int argc, char* argv[]) {
+   int my_rank, p;   // rank, number processes
+   char g_i;         // holds either g or i depending on user input
+   int *local_A;     // local list: size of local number of elements * size of int
+   int global_n;     // number of elements in global list
+   int local_n;      // number of elements in local list (process list)
+   MPI_Comm comm;
+   double start, finish, loc_elapsed, elapsed;
 
-void doTest(int fullSize, int worldSize){
-  float startTime;
-  int worldRank;
-  int* arrayToSort;
-  int size = fullSize / worldSize;
-  int *sorted;
-  arrayToSort = randomArray(arrayToSort, fullSize);
-  printArray(arrayToSort, fullSize);
+   MPI_Init(&argc, &argv);
+   comm = MPI_COMM_WORLD;
+   MPI_Comm_size(comm, &p);
+   MPI_Comm_rank(comm, &my_rank);
 
+   Get_args(argc, argv, &global_n, &local_n, &g_i, my_rank, p, comm);
+   local_A = (int*) malloc(local_n*sizeof(int));
 
-  // Initialize MPI
-  MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
+   // generate random list based on user input
+   if (g_i == 'g') {
+      Generate_list(local_A, local_n, my_rank);
+      Print_local_lists(local_A, local_n, my_rank, p, comm);
+   }
+   // read in user defined list from command line
+   else {
+      Read_list(local_A, local_n, my_rank, p, comm);
+//#     ifdef DEBUG
+      Print_local_lists(local_A, local_n, my_rank, p, comm);
+//#     endif
+   }
 
-  if(worldRank == 0) {
-    sorted = malloc(fullSize * sizeof(int));
-  }
+#  ifdef DEBUG
+   printf("Proc %d > Before Sort\n", my_rank);
+   fflush(stdout);
+#  endif
 
-  startTime = (float)clock() / CLOCKS_PER_SEC;
-  mpiSort(worldRank, size, arrayToSort, sorted);
+   MPI_Barrier(comm);
+   start = MPI_Wtime();
+   Sort(local_A, local_n, my_rank, p, comm);
+   finish = MPI_Wtime();
+   loc_elapsed = finish-start;
+   MPI_Reduce(&loc_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
 
-  printArray(arrayToSort, fullSize);
+#  ifdef DEBUG
+   Print_local_lists(local_A, local_n, my_rank, p, comm);
+   fflush(stdout);
+#  endif
 
-  // Make the final mergeSort call
-  if(worldRank == 0) {
-    int *otherArray = malloc(fullSize * sizeof(int));
+   Print_global_list(local_A, local_n, my_rank, p, comm);
 
+   free(local_A);  // deallocate memory
 
+   if (my_rank == 0) printf("Sorting took %f milliseconds \n", loc_elapsed*1000);
 
-    float timeElapsed = (float)clock() / CLOCKS_PER_SEC - startTime;
+   MPI_Finalize();
 
-    char newFilePath[50];
-    sprintf(newFilePath,"zans.txt");
-
-    FILE *fptr = fopen(newFilePath, "a");
-    if(fptr  == NULL){
-      printf("Can't open the file\n");
-    }
-
-    isCorrect(sorted, fullSize);
-    printArray(sorted, fullSize);
-    fprintf(fptr ,"Num of Proccesses: %i | Size %i | Time %f \n", worldSize, fullSize, timeElapsed);
-
-    fclose(fptr);
-    free(sorted);
-    free(otherArray);
-  }
-
-  printf("WorldRank %d\n", worldRank);
-  free(arrayToSort);
-  MPI_Barrier(MPI_COMM_WORLD);
-}
-
-int main(int argc, char** argv) {
-  int fullSize = atoi(argv[1]);
-  int print = atoi(argv[2]);
-  int worldSize;
-  //char *fileName;
-
-
-  MPI_Init(NULL, NULL);
-  MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
-  // fileName = getFileName(worldSize);
-
-  //doTest(10000000, worldSize);
-  doTest(10, worldSize);
-  //doTest(20000000, worldSize);
-  //doTest(30000000, worldSize);
-  //doTest(40000000, worldSize);
-  //doTest(50000000, worldSize);
-  //doTest(60000000, worldSize);
-
-  MPI_Finalize();
-}
+   return 0;
+}  /* main */
